42d766b1939ebc00010102d664132b78ee5c73d3
pedrovgc <pedrovgc@policiacivil.go.gov.br>
2024-06-10 08:35:37 -0300

    teste ssows prod

--- src/configs/urlsConfig.ts
+++ src/configs/urlsConfig.ts
@@ -1,4 +1,4 @@
-import { domainNameProd, domainNameDesv } from './sistemaConfig';
+import { domainNameDesv, domainNameProd } from './sistemaConfig';
 export const urlsServices = {
   SIGUWS: 'https://siguws.ssp.go.gov.br/',
   LEGADOWS: 'https://legadows.ssp.go.gov.br/',
@@ -16,13 +16,13 @@ if (window.location.hostname.indexOf(domainNameProd) > -1) {
 } else {
   urlsServices.SIGUWS = 'https://siguws-h.ssp.go.gov.br/';
   urlsServices.LEGADOWS = 'https://legadows-h.ssp.go.gov.br/';
-  urlsServices.SSOWS = 'https://ssows-h.ssp.go.gov.br/';
+  urlsServices.SSOWS = 'https://ssows.ssp.go.gov.br/';
   urlsServices.BACKENDWS = 'https://progapws-homo.policiacivil.go.gov.br';
   ambiente = 'HOMO';
   if (window.location.hostname.indexOf(domainNameDesv) > -1) {
     //PARA DESENVOLVIMENTO
     urlsServices.BACKENDWS = 'http://localhost:3000';
-    urlsServices.SSOWS = 'https://ssows-h.ssp.go.gov.br/';
+    urlsServices.SSOWS = 'https://ssows.ssp.go.gov.br/';
     //urlsServices.BACKENDWS = 'https://progapws-homo.policiacivil.go.gov.br'
     // urlsServices.BACKEND = 'URLBACKENDHOMO'
   }
