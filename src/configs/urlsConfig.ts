3c69e59d68b3b17d294b3d295133f41ba5033bfc
pedrovgc <pedrovgc@policiacivil.go.gov.br>
2024-06-10 09:16:08 -0300

    teste prod

--- src/configs/urlsConfig.ts
+++ src/configs/urlsConfig.ts
@@ -9,6 +9,7 @@ export let ambiente = '';
 
 if (window.location.hostname.indexOf(domainNameProd) > -1) {
   console.log('teste 1 - ', domainNameProd);
+  console.log('teste 1 -', window.location.hostname.indexOf(domainNameProd));
   urlsServices.SIGUWS = 'https://siguws.ssp.go.gov.br/';
   urlsServices.LEGADOWS = 'https://legadows.ssp.go.gov.br/';
   urlsServices.SSOWS = 'https://ssows.ssp.go.gov.br/';
@@ -16,6 +17,7 @@ if (window.location.hostname.indexOf(domainNameProd) > -1) {
   ambiente = 'PROD';
 } else {
   console.log('teste 2 - ', domainNameProd);
+  console.log('teste 2 - ', window.location.hostname.indexOf(domainNameProd));
 
   urlsServices.SIGUWS = 'https://siguws-h.ssp.go.gov.br/';
   urlsServices.LEGADOWS = 'https://legadows-h.ssp.go.gov.br/';
